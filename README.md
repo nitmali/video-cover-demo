05fca9efce619a655d1f7fae735d9f10468f37ce
MaLi <me@nitmali.com>
2021-02-09 17:12:34 +0800

    从网络视频获取封面图

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## 从视频截取封面图的demo
 
 ### 相关依赖：[javacv-platform](https://github.com/bytedeco/javacv)
+
+### demo自带了个本地视频文件，稍微有点大
--- src/main/java/AppMain.java
+++ src/main/java/AppMain.java
@@ -4,7 +4,6 @@ import org.bytedeco.javacv.Java2DFrameConverter;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 
 public class AppMain {
@@ -17,12 +16,42 @@ public class AppMain {
     }
 
     /**
-     * 获取视频封面
+     * 获取本地视频封面
      * @param videoPath 视频path
      * @param savePath 封面保存path
      */
     public static void getCover(String videoPath, String savePath) throws Exception {
         FFmpegFrameGrabber fFmpegFrameGrabber = FFmpegFrameGrabber.createDefault(new File(videoPath));
+        fFmpegFrameGrabber.start();
+        int length = fFmpegFrameGrabber.getLengthInFrames();
+        int i = 0;
+        Frame frame = null;
+        while (i < length) {
+            frame = fFmpegFrameGrabber.grabFrame();
+            // 为了防止出现黑屏的封面图，从第6帧开始取
+            if ((i > 6) && (frame.image != null)) {
+                break;
+            }
+            i++;
+        }
+        if (frame == null) {
+            return;
+        }
+        Java2DFrameConverter converter = new Java2DFrameConverter();
+        BufferedImage bufferedImage = converter.getBufferedImage(frame);
+        File file = new File(savePath + "cover.png");
+        ImageIO.write(bufferedImage, "png", file);
+        fFmpegFrameGrabber.stop();
+    }
+
+    /**
+     * 获取网络视频封面
+     * @param videoPath 网络视频path
+     * @param savePath 封面保存path
+     */
+    public static void getCoverByNetwork(String videoPath, String savePath) throws Exception {
+        FFmpegFrameGrabber fFmpegFrameGrabber = new FFmpegFrameGrabber(videoPath);
+        // 下面都是一样的
         fFmpegFrameGrabber.start();
         int length = fFmpegFrameGrabber.getLengthInFrames();
         int i = 0;
