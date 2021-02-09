a7f77260400c4b11325c7ab98f684020e3900040
MaLi <me@nitmali.com>
2021-02-09 17:01:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## 从视频截取封面图的demo
+
+### 相关依赖：[javacv-platform](https://github.com/bytedeco/javacv)
---
+++ src/main/java/AppMain.java
@@ -0,0 +1,47 @@
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.Frame;
+import org.bytedeco.javacv.Java2DFrameConverter;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+public class AppMain {
+    public static void main(String[] args) {
+        try {
+            getCover("src/main/resources/8_15488489004165.mp4", "./");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * 获取视频封面
+     * @param videoPath 视频path
+     * @param savePath 封面保存path
+     */
+    public static void getCover(String videoPath, String savePath) throws Exception {
+        FFmpegFrameGrabber fFmpegFrameGrabber = FFmpegFrameGrabber.createDefault(new File(videoPath));
+        fFmpegFrameGrabber.start();
+        int length = fFmpegFrameGrabber.getLengthInFrames();
+        int i = 0;
+        Frame frame = null;
+        while (i < length) {
+            frame = fFmpegFrameGrabber.grabFrame();
+            // 为了防止出现黑屏的封面图，从第6帧开始取
+            if ((i > 6) && (frame.image != null)) {
+                break;
+            }
+            i++;
+        }
+        if (frame == null) {
+            return;
+        }
+        Java2DFrameConverter converter = new Java2DFrameConverter();
+        BufferedImage bufferedImage = converter.getBufferedImage(frame);
+        File file = new File(savePath + "cover.png");
+        ImageIO.write(bufferedImage, "png", file);
+        fFmpegFrameGrabber.stop();
+    }
+}
